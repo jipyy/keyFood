48119e0c6d3c70bd5ccb8457fdc9289015deabfb
Andika <andikasupriyadi27@gmail.com>
2024-07-05 14:46:22 +0700

    sidebar by Andika, belum 100%

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,84 @@
+<nav class="sidebar close">
+    <header>
+        <div class="image-text">
+            <span class="image">
+                <img src="{{ asset('public/img/logo.jpg') }}" alt="logo">
+            </span>
+
+            <div class="text-header-text">
+                <span class="name">KeyFood</span>
+                <span class="email">Keyfood@gmail.com</span>
+            </div>
+        </div>
+
+        <i class="bx bx-chevron-right toggle"></i>
+    </header>
+    <div class="menu-bar">
+        <div class="menu">
+
+            <a href="">
+                <li class="search-box">
+                    <i class="bx bx-search icon"></i>
+                    <input type="search" placeholder="search..">
+                </li>
+            </a>
+
+            <ul class="menu-links">
+                <li class="nav-link">
+                    <a href="">
+                        <i class="bx bx-home-alt icon"></i>
+                        <span class="text nav-text">Home</span>
+                    </a>
+                </li>
+                <li class="nav-link">
+                    <a href="">
+                        <i class="bx bx-bar-chart-alt-2 icon"></i>
+                        <span class="text nav-text">Product</span>
+                    </a>
+                </li>
+                <li class="nav-link">
+                    <a href="">
+                        <i class="bx bx-home-alt icon"></i>
+                        <span class="text nav-text">Stores</span>
+                    </a>
+                </li>
+                <li class="nav-link">
+                    <a href="">
+                        <i class="bx bx-home-alt icon"></i>
+                        <span class="text nav-text">Categories</span>
+                    </a>
+                </li>
+                <li class="nav-link">
+                    <a href="">
+                        <i class="bx bx-home-alt icon"></i>
+                        <span class="text nav-text">Notifications</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="bottom-content">
+            <li class="">
+                <a href="">
+                    <i class="bx bx-log-out icon"></i>
+                    <span class="text nav-text">Logout</span>
+                </a>
+            </li>
+
+            <li class="mode">
+               <div class="moon-sun">
+                <i class="bx bx-moon icon moon"></i>
+                <i class="bx bx-sun icon sun"></i>
+               </div>
+               <span class="mode-text text">Dark  Mode</span>
+               <div class="toggle-switch">
+                <span class="switch"></span>
+               </div>
+            </li>
+        </div>
+    </div>
+</nav>
+
+<script src="sidebar.js">
+
+</script>
