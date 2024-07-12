c9363507d3fe4c50b0a49d936e9dbf7041662223
jipyy <jahdanpandita16@gmail.com>
2024-07-12 10:20:20 +0700

    hgnkcwdmwds

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,42 @@
 
+<form method="POST" action="{{ route('login') }}" class="sign-in-form">
+    @csrf
+    <h2 class="title">
+      <img src="img/login.svg" style="width: 150px; align-items: center;" alt="">
+    </h2>
+    <div class="input-field">
+      <i class="fas fa-envelope"></i>
+      <input type="email" placeholder="Email" name="email" required />
+    </div>
+    <div class="input-field">
+      <i class="fas fa-lock"></i>
+      <input type="password" placeholder="Password" name="password" required />
+    </div>
+    <input type="submit" value="Login" class="btn solid" />
+    <p class="social-text">Or Sign in with social platforms</p>
+    <div class="social-media">
+      <a href="#" class="social-icon">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="#" class="social-icon">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="#" class="social-icon">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="#" class="social-icon">
+        <i class="fab fa-linkedin-in"></i>
+      </a>
+    </div>
+  </form>
+
+
+  {{-- Ini adalah form bawaan laravel breeze --}}
+
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
@@ -42,36 +80,3 @@
         </div>
     </form>
 </x-guest-layout>
-
-{{-- <div class="container">
-    <div class="forms-container">
-      <div class="signin-signup">
-        <form action="#" class="sign-in-form">
-          <h2 class="title">
-            <img src="img/login.svg" style="width: 150px; align-items: center;" alt="">
-          </h2>
-          <div class="input-field">
-            <i class="fas fa-envelope"></i>
-            <input type="email" placeholder="Email" name="email" />
-          </div>
-          <div class="input-field">
-            <i class="fas fa-lock"></i>
-            <input type="password" placeholder="Password" />
-          </div>
-          <input type="submit" value="Login" class="btn solid" />
-          <p class="social-text">Or Sign in with social platforms</p>
-          <div class="social-media">
-            <a href="#" class="social-icon">
-              <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="#" class="social-icon">
-              <i class="fab fa-twitter"></i>
-            </a>
-            <a href="#" class="social-icon">
-              <i class="fab fa-google"></i>
-            </a>
-            <a href="#" class="social-icon">
-              <i class="fab fa-linkedin-in"></i>
-            </a>
-          </div>
-        </form> --}}
