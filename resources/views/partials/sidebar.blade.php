d2826f3fdd6d4525fcb818f5698a155775c52bc2
Andika <andikasupriyadi27@gmail.com>
2024-07-16 10:35:51 +0700

    update profile by Andika

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -5,23 +5,29 @@
                 <img src="{{ asset('../img/logo.png') }}" alt="logo">
             </span>
 
-            <div class="text-header-text hidden">
-                <span class="name">KeyFood</span>
-                <span class="email">Keyfood@gmail.com</span>
-            </div>
             <div class="text-header-text">
-                <span class="name"></span>
-                <span class="email">
-                    <a href="/log-reg">
-                        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
-                            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                            Login
-                            </span>
-                            <div>{{ Auth::user()->name }}</div>
-                            <div>{{ Auth::user()->email }}</div>
-                            </button>
-                    </a>
+                @if (Auth::check())
+                <span class="name">
+                    <div>{{ Auth::user()->name }}</div>
+
                 </span>
+                <span class="email">
+                    <div>{{ Auth::user()->email }}</div>
+
+                </span>
+                @else
+                    <span class="email">
+                        <a href="/log-reg">
+                            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                Login
+                                </span>
+                               
+                                </button>
+                        </a>
+                    </span>
+                @endif
+            </div>
             </div>
         </div>
 
@@ -82,8 +88,8 @@
                         @csrf
                         <span class="text nav-text">Logout</span>
                     </a>
+                </form>
             </li>
-            </form>
 
             <li class="mode">
                 <div class="moon-sun">
