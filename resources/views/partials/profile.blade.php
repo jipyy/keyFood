348f46e083eb9fd521d5d1405dcbea8244482ea9
Andika <andikasupriyadi27@gmail.com>
2024-07-25 09:04:31 +0700

    "Updated CSS files (home.css, load.css, style.css) with new styles and animations, removed delete account form from edit-profile.blade.php and added it to profile.blade.php, and updated footer.blade.php with new SVG wave design

--- resources/views/partials/profile.blade.php
+++ resources/views/partials/profile.blade.php
@@ -145,6 +145,13 @@
             <a href="{{ route('profile.edit') }}">
                 <button class="download-btn"><i class="fa fa-edit"></i> Edit Profile</button>
             </a>
+            <form action="{{ route('profile.destroy') }}" method="POST" class="mt-6">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                    Delete Account
+                </button>
+            </form>
             {{-- <a href="{{ route('coba.edit') }}">
                 <button class="download-btn"><i class="fa fa-edit"></i> Edit Profile123</button>
             </a> --}}
