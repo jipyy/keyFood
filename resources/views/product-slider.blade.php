a79d354c6c9028f4b27e67eae03c78356c63e8ef
MuhammadFiqih17 <fiqihsetiawan2617@gmail.com>
2024-07-31 09:41:01 +0700

    store ke localstorage

--- resources/views/product-slider.blade.php
+++ resources/views/product-slider.blade.php
@@ -14,9 +14,6 @@
                         <strong>{{ $product->name }}</strong>
                         <span class="quantity"></span>
                         <span class="price">Rp {{ number_format($product->price, 0, ',', '.') }}</span>
-                        {{-- <a href="{{ route('add.to.cart', $product->id) }}" class="cart-btn">
-                            <i class="fas fa-shopping-bag"></i> Add Cart
-                        </a> --}}
                         <a href="javascript:void(0)" data-product-id="{{ $product->id }}" class="cart-btn">
                             <i class="fas fa-shopping-bag"></i> Add Cart
                         </a>
@@ -29,87 +26,89 @@
 
             @include('partials.cart')
             <div class="pagination">
-                {{-- Menampilkan link pagination --}}
                 {{ $products->links() }}
             </div>
         </section>
     </section>
     <script>
         function getData() {
-            $.ajax({
-                url: '/data',
-                type: 'GET',
-                success: function(res) {
-                    if(res.data == null){
-                        document.getElementById('cart').innerHTML = '<p>Your cart is empty.</p>'
-                        document.getElementById('count-cart').innerHTML = 0
-                        document.getElementById('total-price').innerHTML = 'Rp 0'
-                    } else {
-                        let totalPrice = 0
-                        document.getElementById('count-cart').innerHTML = Object.keys(res.data).length
-                        document.getElementById('cart').innerHTML = ""
-                        for (let key in res.data) {
-                            let itemTotal = res.data[key].price * res.data[key].quantity
-                            totalPrice += itemTotal
-                            document.getElementById('cart').innerHTML += `
-                                <div class="cart-item">
-                                    <img src="${res.data[key].photo}" alt="${res.data[key].name}">
-                                    <h5>${res.data[key].name}</h5>
-                                    <span class="price">Rp ${itemTotal.toLocaleString('id-ID')}</span>
-                                    <p>${res.data[key].quantity}</p>
-                                    <a href="javascript:void(0)" class="decrement" data-decrement-id="${key}">-</a>
-                                    <a href="javascript:void(0)" class="remove" data-remove-id="${key}">Remove</a>
-                                </div>`
-                        }
-                        document.getElementById('total-price').innerHTML = 'Rp ' + totalPrice.toLocaleString('id-ID')
-                    }
+            let cart = JSON.parse(localStorage.getItem('cart')) || {};
+            if (Object.keys(cart).length === 0) {
+                document.getElementById('cart').innerHTML = '<p>Your cart is empty.</p>';
+                document.getElementById('count-cart').innerHTML = 0;
+                document.getElementById('total-price').innerHTML = 'Rp 0';
+            } else {
+                let totalPrice = 0;
+                document.getElementById('count-cart').innerHTML = Object.keys(cart).length;
+                document.getElementById('cart').innerHTML = '';
+                for (let key in cart) {
+                    let itemTotal = cart[key].price * cart[key].quantity;
+                    totalPrice += itemTotal;
+                    document.getElementById('cart').innerHTML += `
+                        <div class="cart-item">
+                            <img src="${cart[key].photo}" alt="${cart[key].name}">
+                            <h5>${cart[key].name}</h5>
+                            <span class="price">Rp ${itemTotal.toLocaleString('id-ID')}</span>
+                            <p>${cart[key].quantity}</p>
+                            <a href="javascript:void(0)" class="decrement" data-decrement-id="${key}">-</a>
+                            <a href="javascript:void(0)" class="remove" data-remove-id="${key}">Remove</a>
+                        </div>`;
                 }
-            })
+                document.getElementById('total-price').innerHTML = 'Rp ' + totalPrice.toLocaleString('id-ID');
+            }
         }
 
         $(function() {
-            getData()
-        })
+            getData();
+        });
 
         $(document).ready(function() {
             $('.cart-btn').on('click', function() {
-                let productId = $(this).data('product-id')
+                let productId = $(this).data('product-id');
+                let cart = JSON.parse(localStorage.getItem('cart')) || {};
+                
+                if (cart[productId]) {
+                    cart[productId].quantity++;
+                } else {
+                    cart[productId] = {
+                        name: $(this).siblings('strong').text(),
+                        price: parseFloat($(this).siblings('.price').text().replace('Rp ', '').replace(/\./g, '')),
+                        photo: $(this).siblings('img').attr('src'),
+                        quantity: 1
+                    };
+                }
+                
+                localStorage.setItem('cart', JSON.stringify(cart));
+                getData();
+            });
 
-                $.ajax({
-                    url: '/cart/add/' + productId,
-                    type: 'GET',
-                    success: function(res) {
-                        getData()
-                    }
-                })
-            })
-
-            // Delegasi event untuk decrement dan remove
             $(document).on('click', '.decrement', function() {
-                let productId = $(this).data('decrement-id')
-                $.ajax({
-                    url: "/cart/decrement/" + productId,
-                    type: 'GET',
-                    success: function(res) {
-                        console.log('Success Decrease')
-                        getData()
-                    },
-                    error: function(err) {
-                        console.log(err)
+                let productId = $(this).data('decrement-id');
+                let cart = JSON.parse(localStorage.getItem('cart')) || {};
+
+                if (cart[productId]) {
+                    if (cart[productId].quantity > 1) {
+                        cart[productId].quantity--;
+                    } else {
+                        delete cart[productId];
                     }
-                })
-            })
+                }
+
+                localStorage.setItem('cart', JSON.stringify(cart));
+                getData();
+            });
 
             $(document).on('click', '.remove', function() {
-                let productId = $(this).data('remove-id')
-                $.ajax({
-                    url: "/cart/remove/" + productId,
-                    type: 'GET',
-                    success: function(res) {
-                        getData()
-                    }
-                })
-            })
-        })
+                let productId = $(this).data('remove-id');
+                let cart = JSON.parse(localStorage.getItem('cart')) || {};
+
+                if (cart[productId]) {
+                    delete cart[productId];
+                }
+
+                localStorage.setItem('cart', JSON.stringify(cart));
+                getData();
+            });
+        });
     </script>
 @endsection
