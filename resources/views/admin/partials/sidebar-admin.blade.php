464eba32e1df0adbc1dd64102976bcc3f1bbed06
Andika <andikasupriyadi27@gmail.com>
2024-08-06 13:39:46 +0700

    "Removed 'admin/' prefix from route checks and hrefs in sidebar-admin.blade.php"

--- resources/views/admin/partials/sidebar-admin.blade.php
+++ resources/views/admin/partials/sidebar-admin.blade.php
@@ -61,12 +61,12 @@
 
             <ul class="list-none">
                 <li class="relative px-6 py-3">
-                    @if (Request::is('admin/role-requests'))
+                    @if (Request::is('role-requests'))
                         <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
                             aria-hidden="true"></span>
                     @endif
-                    <a class="{{ Request::is('admin/role-requests') ? 'inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100' : 'inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200' }}"
-                        href="/admin/role-requests">
+                    <a class="{{ Request::is('role-requests') ? 'inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100' : 'inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200' }}"
+                        href="/role-requests">
                         <i class='bx bx-check-circle bx-sm'></i>
                         <span class="ml-4">Role Requests</span>
                     </a>
