eee145a556b41003a8706428fe3204a8de9640f4
Andika <andikasupriyadi27@gmail.com>
2024-08-20 15:35:01 +0700

    "Commented out entire clock and dark/light theme JavaScript code in public/js/clock.js and modified database migration to change 'tanggal_order' column type from integer to date in orders table."

--- database/migrations/2024_08_06_220148_create_orders_table.php
+++ database/migrations/2024_08_06_220148_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('no_order');
-            $table->integer('tanggal_order');
+            $table->date('tanggal_order');
             $table->integer('quantity');
             $table->string('photo');
             $table->date('order_date');
