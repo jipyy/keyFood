6348eab67b25576950fb6be1945f737caf07f0f9
fauzan3l <rumbihgti@gmail.com>
2024-08-20 14:08:17 +0700

    z

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -1,9 +1,9 @@
 @extends('admin.layouts.main-admin')
-
 @section('container-admin')
-    <div class="py-12">
+<main class="h-screen overflow-y-auto">
+<div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden pl-10 shadow-sm sm:rounded-lg p-6">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden pl-10 shadow-sm sm:rounded-lg p-6">
                 @if($errors->any())
                 <div class="alert alert-danger mb-4">
                     <ul>
@@ -20,22 +20,22 @@
                     @csrf
 
                     <div class="mb-4">
-                        <label for="name" class="block text-gray-700 font-bold mb-2">Name</label>
-                        <input type="text" id="name" name="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                        <label for="name" class="block text-gray-700 dark:text-gray-300 font-bold mb-2">Name</label>
+                        <input type="text" id="name" name="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
                     </div>
 
                     <div class="mb-4">
-                        <label for="slug" class="block text-gray-700 font-bold mb-2">Slug</label>
-                        <input type="text" id="slug" name="slug" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                        <label for="slug" class="block text-gray-700 dark:text-gray-300 font-bold mb-2">Slug</label>
+                        <input type="text" id="slug" name="slug" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
                     </div>
 
                     <div class="mb-4">
-                        <label for="icon" class="block text-gray-700 font-bold mb-2">Icon</label>
-                        <input type="file" id="icon" name="icon" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                        <label for="icon" class="block text-gray-700 dark:text-gray-300 font-bold mb-2">Icon</label>
+                        <input type="file" id="icon" name="icon" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
                     </div>
 
                     <div class="flex items-center justify-between">
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        <button type="submit" class="bg-blue-500 dark:bg-blue-700 hover:bg-blue-700 dark:hover:bg-blue-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                             Save
                         </button>
                     </div>
@@ -43,4 +43,5 @@
             </div>
         </div>
     </div>
+</main>
 @endsection
