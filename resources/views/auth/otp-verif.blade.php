85745d17ed92f3655b8d30816e1d261eb2d6b0eb
Andika <andikasupriyadi27@gmail.com>
2024-08-29 13:20:02 +0700

    update

--- resources/views/auth/otp-verif.blade.php
+++ resources/views/auth/otp-verif.blade.php
@@ -15,47 +15,57 @@
     <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
     <link rel="icon" type="image/x-icon" href="{{ asset('../img/whatsapp.png') }}">
 
+    <style>
+        /* Hide spinner for Chrome, Safari, and newer versions of Edge */
+        .no-spinner::-webkit-inner-spin-button,
+        .no-spinner::-webkit-outer-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+    </style>
+
 </head>
 
-<body class="bg-cover bg-center" style="background-image: url('../img/otp-bg.jpg');">
-    <div class="max-w-md mx-auto my-20 max-lg:my-40 text-center bg-white bg-opacity-30 backdrop-blur-lg px-4 sm:px-8 py-10 rounded-xl shadow-lg">
-        <header class="mb-8">
-            <h1 class="text-2xl font-bold mb-1">Mobile Phone Verification</h1>
-            <p class="text-[15px] text-slate-500">Enter the 6-digit verification code that was sent to your phone
-                number.</p>
-        </header>
-        <form id="otp-form" method="POST" action="{{ route('verify.wa.otp') }}">
-            @csrf
-            <div class="flex flex-wrap items-center justify-center gap-2 md:gap-3">
-                @for ($i = 0; $i < 6; $i++)
-                    <input type="number"
-                        class="w-12 h-12 text-center text-lg font-extrabold text-slate-900 bg-slate-100 border border-transparent hover:border-slate-200 appearance-none rounded p-2 outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100 
-                               sm:w-14 sm:h-14 sm:text-xl md:w-16 md:h-16 md:text-2xl"
-                        pattern="\d*" maxlength="1" name="otp[]" required autofocus />
-                @endfor
+<body class="bg-cover bg-center bg-no-repeat w-full h-screen" style="background-image: url('../img/otp-bg.jpg');">
+    <section class="max-sm:py-64 md:py-24">
+        <div
+            class="max-w-md max-sm:max-w-sm max-h-lg mx-auto my-auto text-center bg-white bg-opacity-30 backdrop-blur-lg px-4 sm:px-8 py-10 rounded-xl shadow-lg">
+            <header class="mb-8">
+                <h1 class="text-2xl font-bold mb-1">Mobile Phone Verification</h1>
+                <p class="text-[15px] text-slate-100">Enter the 6-digit verification code that was sent to your phone
+                    number.</p>
+            </header>
+            <form id="otp-form" method="POST" action="{{ route('verify.wa.otp') }}">
+                @csrf
+                <div class="flex items-center justify-center gap-3">
+                    @for ($i = 0; $i < 6; $i++)
+                        <input type="number"
+                            class="no-spinner w-14 h-14 text-center text-2xl font-extrabold text-slate-900 bg-slate-100 border border-transparent hover:border-slate-200 appearance-none rounded p-4 outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100 max-sm:w-12 max-sm:h-12 max-sm:text-xl max-sm:p-2"
+                            pattern="\d*" maxlength="1" name="otp[]" required autofocus />
+                    @endfor
+                </div>
+
+
+                @error('otp')
+                    <span>{{ $message }}</span>
+                @enderror
+                <div class="max-w-[260px] mx-auto mt-4">
+                    <button type="submit"
+                        class="w-full inline-flex justify-center whitespace-nowrap rounded-lg bg-indigo-500 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus:outline-none focus:ring focus:ring-indigo-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 transition-colors duration-150">
+                        Verify Account
+                    </button>
+                </div>
+            </form>
+
+            <div class="text-sm text-slate-500 mt-4">
+                Didn't receive code?
+                <span id="countdown"></span>
+                <a id="resend-link" class="font-medium text-indigo-500 hover:text-indigo-600 hidden"
+                    href="{{ route('resend.otp') }}">Resend</a>
             </div>
 
-            @error('otp')
-                <span>{{ $message }}</span>
-            @enderror
-            <div class="max-w-[260px] mx-auto mt-4">
-                <button type="submit"
-                    class="w-full inline-flex justify-center whitespace-nowrap rounded-lg bg-indigo-500 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus:outline-none focus:ring focus:ring-indigo-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 transition-colors duration-150">
-                    Verify Account
-                </button>
-            </div>
-        </form>
-
-        <div class="text-sm text-slate-500 mt-4">
-            Didn't receive code?
-            <span id="countdown"></span>
-            <a id="resend-link" class="font-medium text-indigo-500 hover:text-indigo-600 hidden"
-                href="{{ route('resend.otp') }}">Resend</a>
         </div>
-
-    </div>
-
-
+    </section>
 
     <script>
         document.addEventListener('DOMContentLoaded', function() {
