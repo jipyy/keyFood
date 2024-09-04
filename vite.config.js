c1f3765503cb952ae05b53a7b414e92046fece62
jipyy <jahdanpandita16@gmail.com>
2024-09-04 15:21:47 +0700

    update vite jadi 2 opsi

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     server: {
-        host: '172.16.16.213', // Mengizinkan akses dari semua IP
-        port: 8000, // Sesuaikan port dengan yang Anda gunakan
+        host: '0.0.0.0', // Atau 'localhost' jika ingin spesifik ke localhost
+        port: 8000, // Pastikan port ini tidak digunakan oleh aplikasi lain`
+
+
+        // PORT UNTUK TES MELALUI IP WIFI
+        
+        // host: '172.16.16.213', 
+        // port: 8000, 
       },
     plugins: [
         laravel({
