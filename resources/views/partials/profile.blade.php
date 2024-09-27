e939a26cfe36a524175363f077be21cebe78ab00
jipyy <jahdanpandita16@gmail.com>
2024-09-27 14:14:59 +0700

    update

--- resources/views/partials/profile.blade.php
+++ resources/views/partials/profile.blade.php
@@ -87,20 +87,17 @@
                 </div>
                 <form method="POST" action="{{ route('logout') }}" style="display: flex">
                     @csrf
-                    <a  onclick="event.preventDefault();
-            this.closest('form').submit();">
-                        <button id="logout"
-                            class="flex justify-center gap-3 rounded-md bg-red-600 py-2 px-3 font-semibold hover:bg-red-500 focus:ring-2 focus:ring-red-400">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                class="h-6 w-6">
-                                <path fill-rule="evenodd"
-                                    d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm10.72 4.72a.75.75 0 011.06 0l3 3a.75.75 0 010 1.06l-3 3a.75.75 0 11-1.06-1.06l1.72-1.72H9a.75.75 0 010-1.5h10.94l-1.72-1.72a.75.75 0 010-1.06z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                            <span>Logout</span>
-                        </button>
-                    </a>
-                </form>
+                    <button id="logout"
+                        class="flex justify-center gap-3 rounded-md bg-red-600 py-2 px-3 font-semibold hover:bg-red-500 focus:ring-2 focus:ring-red-400"
+                        onclick="event.preventDefault(); this.closest('form').submit();">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6">
+                            <path fill-rule="evenodd"
+                                d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm10.72 4.72a.75.75 0 011.06 0l3 3a.75.75 0 010 1.06l-3 3a.75.75 0 11-1.06-1.06l1.72-1.72H9a.75.75 0 010-1.5h10.94l-1.72-1.72a.75.75 0 010-1.06z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                        <span>Logout</span>
+                    </button>
+                </form>                
             </div>
         @else
             <p>You Haven't Login,</p>
