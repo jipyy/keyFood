66d85b9163d1be7a9bae2b41ab7e19316d341dc2
jipyy <jahdanpandita16@gmail.com>
2024-10-01 10:47:28 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -147,7 +147,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
     // Route::get('/history', [AdminHistoryController::class, 'index'])->name('history.index')->middleware('permission:histories');
 
-    Route::resource('history', AdminHistoryController::class)->middleware('permission:histories');
+    Route::resource('history', AdminHistoryController::class)->middleware('role:admin');
 
     Route::resource('faqs', FaqController::class)->middleware('permission:faqs');
 
