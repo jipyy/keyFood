d3c06059508d320e13e209f848f6645821714bfd
jipyy <jahdanpandita16@gmail.com>
2024-10-01 10:55:38 +0700

    update

--- resources/views/admin/partials/sidebar-admin.blade.php
+++ resources/views/admin/partials/sidebar-admin.blade.php
@@ -233,11 +233,11 @@
             </ul>
             <ul class="list-none">
                 <li class="relative px-6 py-3">
-                    @if (Request::is('admin/history'))
+                    @if (Request::is('/admin/history'))
                         <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
                             aria-hidden="true"></span>
                     @endif
-                    <a class="{{ Request::is('admin/history') ? 'inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100' : 'inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200' }}"
+                    <a class="{{ Request::is('/admin/history') ? 'inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100' : 'inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200' }}"
                         href="/admin/history">
                         <i class='bx bx-history bx-sm'></i>
                         <span class="ml-4">Histories</span>
