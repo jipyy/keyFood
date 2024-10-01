fa8ef2ff6eb17ec889d67edc00f0863f43a6cead
jipyy <jahdanpandita16@gmail.com>
2024-10-01 12:00:08 +0700

    upadte

--- resources/views/admin/partials/sidebar-admin.blade.php
+++ resources/views/admin/partials/sidebar-admin.blade.php
@@ -106,7 +106,7 @@
                             aria-hidden="true"></span>
                     @endif
                     <a class="{{ Request::is('admin/faqs') ? 'inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100' : 'inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200' }}"
-                        href="/admin.faqs.index">
+                        href="/admin/faqs">
                         <i class='bx bx-question-mark bx-sm'></i>
                         <span class="ml-4">FAQ</span>
                     </a>
