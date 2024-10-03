a640b47d2936ecc3bb049174bfb4ce9b5bfa3a82
jipyy <jahdanpandita16@gmail.com>
2024-10-03 12:54:44 +0700

    update

--- resources/views/admin/cms/index.blade.php
+++ resources/views/admin/cms/index.blade.php
@@ -122,7 +122,8 @@
 
                                 <td class="px-4 py-3">
                                     <div class="flex items-center space-x-4 text-sm">
-                                        <a href="{{route('admin.company.edit', $company->id)}}">
+                                        {{-- <a href="{{route('admin.company.edit', $company->id)}}"> --}}
+                                        <a href="/admin/company/edit/{{ $company->id }}">
                                         <button
                                             class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
                                             aria-label="Edit">
