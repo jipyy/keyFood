757b34bcdea71f8aadf79cfc134f49997abf4d2a
jipyy <jahdanpandita16@gmail.com>
2024-10-03 14:32:33 +0700

    update

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -213,7 +213,7 @@
                                         <td class="px-4 py-3 text-sm">
                                             <div class="flex gap-2">
                                                 {{-- <a href="{{ route('admin.users.edit', $user) }}" --}}
-                                                <a href="/admin/users/edit/{{ $user }}"
+                                                <a href="/admin/users/edit/{{ $user->id }}"
                                                     class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray">
                                                     <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
                                                         viewBox="0 0 20 20">
