e25c2a23f6686ebf9f504359660ee1b1c9323119
jipyy <jahdanpandita16@gmail.com>
2024-10-04 14:11:21 +0700

    update

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -63,7 +63,7 @@
                                     </td>
                                     <td class="px-4 py-3">
                                         <div class="flex items-center space-x-4 text-sm">
-                                            <a href="/admin/categories/edit/{{ $category }}"
+                                            <a href="/admin/categories/edit/{{ $category->id }}"
                                                 class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
                                                 aria-label="Edit">
                                                 <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
@@ -73,7 +73,7 @@
                                                     </path>
                                                 </svg>
                                             </a>
-                                            <form method="POST" action="/admin/categories/destroy/{{ $category }}"
+                                            <form method="POST" action="/admin/categories/destroy/{{ $category->id }}"
                                                 onsubmit="return confirmDelete()">
                                                 @csrf
                                                 @method('DELETE')
