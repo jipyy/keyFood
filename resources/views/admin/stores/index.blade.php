7dadd3f4dfed2e7df812912da5be75802403f880
jipyy <jahdanpandita16@gmail.com>
2024-10-04 10:21:47 +0700

    udpate

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -120,7 +120,7 @@
                                         <td class="px-4 py-3">
                                             <div class="flex items-center space-x-4 text-sm">
                                                 {{-- <a href="{{ route('admin.stores.edit', $store) }}" --}}
-                                                <a href="/admin/stores/edit/{{ $store }}"
+                                                <a href="/admin/stores/edit/{{ $store->id_toko }}"
                                                     class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
                                                     aria-label="Edit">
                                                     <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
@@ -130,7 +130,7 @@
                                                         </path>
                                                     </svg>
                                                 </a>
-                                                <form method="POST" action="/admin/stores{{ $store }}"
+                                                <form method="POST" action="/admin/stores/destroy/{{ $store->id_toko }}"
                                                     onsubmit="return confirmDelete()">
                                                     @csrf
                                                     @method('DELETE')
