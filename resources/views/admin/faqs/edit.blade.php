fc471347ec1dac67f13ec1c94075c5326bf0def6
jipyy <jahdanpandita16@gmail.com>
2024-10-04 15:22:59 +0700

    update

--- resources/views/admin/faqs/edit.blade.php
+++ resources/views/admin/faqs/edit.blade.php
@@ -20,8 +20,8 @@
             </div>
             
             <div class="flex items-center justify-between">
-                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                    {{ isset($faq->id) ? 'Update' : 'Create' }}
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                    Save
                 </button>
             </div>
         </form>
